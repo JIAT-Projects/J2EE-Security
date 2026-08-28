62f661826c1258049b6d24b17a7e79f97dda0f1e
Lakkhana Sudham Kalutara <141295595+LakkhanaSudhamKalutara@users.noreply.github.com>
2026-08-28 18:26:05 +0530

    feat: Add security filters for authentication and admin authorization

---
+++ src/main/java/lk/jiat/bcd/security/filter/AdminAuthorizationFilter.java
@@ -0,0 +1,35 @@
+package lk.jiat.bcd.security.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+//@WebFilter(urlPatterns = "/admin/*")
+public class AdminAuthorizationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null){
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+            return;
+        }
+
+        String role = (String) session.getAttribute("role");
+
+      if("ADMIN".equals(role)){
+          filterChain.doFilter(servletRequest, servletResponse);
+
+      }else {
+          request.setAttribute("message","You are not authorized to access this page");
+          request.getRequestDispatcher("/home.jsp").forward(request,response);
+      }
+    }
+}
